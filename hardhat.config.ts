7bb421fdb4fbe11271084f1edc699d0b5006bd61
Livinalt <livinalt@gmail.com>
2024-09-17 16:40:07 +0100

    Setting Up Hardhat Config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
+// import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config({ path: ".env" });
 
-const config: HardhatUserConfig = {
+const ALCHEMY_MAINNET_API_KEY_URL = process.env.ALCHEMY_MAINNET_API_KEY_URL;
+
+module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      forking: {
+        url: ALCHEMY_MAINNET_API_KEY_URL,
+      },
+    },
+  },
+  lockGasLimit: 200000000000,
+  gasPrice: 10000000000,
 };
-
-export default config;
